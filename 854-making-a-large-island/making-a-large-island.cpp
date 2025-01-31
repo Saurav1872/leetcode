b52b33c64e140e511667b6c63263c00206dbef3a
Saurav1872 <109483667+Saurav1872@users.noreply.github.com>
2025-01-31 19:14:54 +0530

    Time: 299 ms (34.41%) | Memory: 188.1 MB (21.51%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -2,58 +2,68 @@ class Solution {
 public:
     int largestIsland(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<int> sizes{0, 0};
-        int idCnt = 2;      // 0 and 1 are reserve as its binary grid
+        vector<int> sizes = {0, 0};
+        int idCnt = 2, ans = 0;
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < m; ++j) {
-                if(grid[i][j] == 1) {
-                    sizes.push_back(dfs(grid, i, j, idCnt++));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    sizes.push_back(bfs(grid, i, j, idCnt++, directions));
                 }
             }
         }
 
-        vector<pair<int, int>> dir = {
-            {0, 1}, {1, 0}, {0, -1}, {-1, 0}
-        };
-
-        int ans = 0;
-
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < m; ++j) {
-                if(grid[i][j] == 0) {
-                    unordered_set<int> s;
-                    for(auto [dx, dy] : dir) {
-                        int ni = i + dx, nj = j + dy;
-                        if(ni < 0 || ni >= grid.size() || nj < 0 || nj >= grid[0].size()) continue;
-                        s.insert(grid[ni][nj]);
-                    }
+        bool hasZero = false;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 0) {
+                    hasZero = true;
+                    unordered_set<int> neighbors;
                     int size = 1;
-                    for(int id : s) {
+
+                    for (auto [dx, dy] : directions) {
+                        int ni = i + dx, nj = j + dy;
+                        if (ni >= 0 && ni < n && nj >= 0 && nj < m &&
+                            grid[ni][nj] > 1) {
+                            neighbors.insert(grid[ni][nj]);
+                        }
+                    }
+
+                    for (int id : neighbors) {
                         size += sizes[id];
                     }
+
                     ans = max(ans, size);
-                    
                 }
             }
         }
 
-
-        return ans == 0 ? n * m : ans;
-
+        return hasZero ? ans : n * m;
     }
 
 private:
-    int dfs(vector<vector<int>>& grid, int i, int j, int id) {
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != 1) {
-            return 0;
-        }
-
+    int bfs(vector<vector<int>>& grid, int i, int j, int id, vector<pair<int, int>>& directions) {
+        int n = grid.size(), m = grid[0].size();
+        queue<pair<int, int>> q;
+        q.push({i, j});
         grid[i][j] = id;
+        int size = 1;
 
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
 
-        return 1 + dfs(grid, i+1, j, id) + dfs(grid, i, j+1, id)
-                 + dfs(grid, i-1, j, id) + dfs(grid, i, j-1, id);
-
+            for (auto [dx, dy] : directions) {
+                int nx = x + dx, ny = y + dy;
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m &&
+                    grid[nx][ny] == 1) {
+                    grid[nx][ny] = id;
+                    q.push({nx, ny});
+                    size++;
+                }
+            }
+        }
+        return size;
     }
 };
