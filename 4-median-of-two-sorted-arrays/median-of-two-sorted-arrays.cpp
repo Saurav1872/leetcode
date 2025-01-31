5b50e8b0f5a2a09de47198b4a0da6f8cd96e7eb0
Saurav1872 <109483667+Saurav1872@users.noreply.github.com>
2025-01-31 10:24:38 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (64.00%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -14,13 +14,12 @@ public:
         while(low <= high) {
             int mid1 = (low+high) / 2;
             int mid2 = leftSize - mid1;
+            
+            int l1 = (mid1 == 0) ? INT_MIN : nums1[mid1 - 1];
+            int l2 = (mid2 == 0) ? INT_MIN : nums2[mid2 - 1];
 
-            int r1 = (mid1 < n1) ? nums1[mid1] : INT_MAX;
-            int r2 = (mid2 < n2) ? nums2[mid2] : INT_MAX;
-
-
-            int l1 = (mid1-1 >= 0) ? nums1[mid1 - 1] : INT_MIN;
-            int l2 = (mid2-1 >= 0) ? nums2[mid2 - 1] : INT_MIN;
+            int r1 = (mid1 == n1) ? INT_MAX : nums1[mid1];
+            int r2 = (mid2 == n2) ? INT_MAX : nums2[mid2];
 
             if(l1 <= r2 && l2 <= r1) {
                 if(n%2 == 1) return max(l1, l2);
